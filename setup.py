1ab70fdcbec8120ea27a306aa23feabb6bf46556
Simon Coulton <simon.coulton@gmail.com>
2014-01-31 16:09:40 +1100

    Moved test requirements to install_extras[test]

--- setup.py
+++ setup.py
@@ -60,6 +60,9 @@ with open(os.path.join(path, 'README.rst')) as f:
 with open(os.path.join(path, 'requirements.txt')) as f:
     requirements = f.read().splitlines()
 
+with open(os.path.join(path, 'requirements-test.txt')) as f:
+    test_requirements = f.read().splitlines()
+
 
 setup(
     name='watson-validators',
@@ -102,6 +105,9 @@ setup(
 
     zip_safe=False,
     install_requires=requirements,
+    extras_require={
+        'test': test_requirements
+    },
 
     cmdclass={
         'test': PyTest,
