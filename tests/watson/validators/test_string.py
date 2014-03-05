bf47f7befe6674a08dcc024034f6cd3e8f173442
Simon Coulton <simon.coulton@gmail.com>
2014-03-05 11:24:17 +1100

    Test coverage

--- tests/watson/validators/test_string.py
+++ tests/watson/validators/test_string.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from pytest import raises
-from watson.validators.string import Length, Required, RegEx
+from watson.validators.string import Length, Required, RegEx, Csrf
 
 
 class TestLength(object):
@@ -8,6 +8,11 @@ class TestLength(object):
     def test_invalid_min_greater_than_max(self):
         with raises(ValueError):
             Length(min=10, max=9)
+
+    def test_none_value(self):
+        validator = Length(min=1, max=2)
+        with raises(ValueError):
+            validator(None)
 
     def test_doesnt_meet_min_requirement(self):
         with raises(ValueError):
@@ -58,3 +63,15 @@ class TestRegex(object):
         with raises(ValueError):
             validator = RegEx('Test')
             validator('Fail')
+
+
+class TestCsrf(object):
+
+    def test_create(self):
+        validator = Csrf(token='1234')
+        assert validator.token == '1234'
+
+    def test_invalid_match(self):
+        validator = Csrf(token='1234')
+        with raises(ValueError):
+            validator('12345')
